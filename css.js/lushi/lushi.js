dbdb1be7f07912e80b7c1dacf58f2d237390636c
1660812937@qq.com <1660812937@qq.com>
2018-11-25 17:21:09 +0800

    lll

--- css.js/lushi/lushi.js
+++ css.js/lushi/lushi.js
@@ -1,36 +1,38 @@
-var box=document.getElementById('wei');
-var boxY=box.offsetTop;
-document.body.onscroll=function(){
-	if(window.pageYOffset>boxY+20){
-		box.style.position='fixed';
-//		box.style.position='sticky';
-	}else{
-		box.style.position='relative';
+var box = document.getElementById('wei');
+var boxY = box.offsetTop;
+document.body.onscroll = function () {
+	if (window.pageYOffset > boxY + 20) {
+		box.style.position = 'fixed';
+		//		box.style.position='sticky';
+	} else {
+		box.style.position = 'relative';
 	}
 }
 
-var diong=document.getElementById('#righttopA');
-var rightA=document.querySelectorAll('#righttopA div');
-var giong=document.getElementById('#righttopB');
-var aw=document.querySelectorAll('#righttopB div');
+var diong = document.getElementById('#righttopA');
+var rightA = document.querySelectorAll('#righttopA div');
+var giong = document.getElementById('#righttopB');
+var aw = document.querySelectorAll('#righttopB div');
 
-for (var i=0 ; i<rightA.length ; i++){
-	rightA[i].index=i;
-//	console.log(rightA[i].index=i);
+for (var i = 0; i < rightA.length; i++) {
+	rightA[i].index = i;
+	//	console.log(rightA[i].index=i);
 }
-for ( var j=0 ; j<aw.length ; j++){
-	aw[j].index=j;
+for (var j = 0; j < aw.length; j++) {
+	aw[j].index = j;
 }
 
-for(i=0 ; i<rightA.length ; i++){
-	rightA[i].onclick=function(){
-		for(var i = 0 ; i<rightA.length ; i++){
-			rightA[i].style.backgroundPositionY=-0 + 'px';
+for (i = 0; i < rightA.length; i++) {
+	rightA[i].onclick = function () {
+		for (var i = 0; i < rightA.length; i++) {
+			rightA[i].style.backgroundPositionY = -0 + 'px';
 		}
-		rightA[this.index].style.backgroundPositionY=(-180) + 'px';
-		for ( var j=0 ; j<aw.length ; j++){
-			aw[j].style.display='none';
+		rightA[this.index].style.backgroundPositionY = (-180) + 'px';
+		for (var j = 0; j < aw.length; j++) {
+			aw[j].style.display = 'none';
 		}
-		aw[this.index].style.display='block';
+		aw[this.index].style.display = 'block';
 	}
 }
+
+//
